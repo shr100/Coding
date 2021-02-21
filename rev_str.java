e7357376116c8287dd20dd113c4a8dc6f431b1f0
Shruthi <rshruthi100@gmail.com>
2021-02-21 14:21:40 -0800

    Add files via upload

---
+++ rev_str.java
@@ -0,0 +1,13 @@
+//Link: https://leetcode.com/problems/reverse-string/
+class Solution {
+    public void reverseString(char[] s) {
+        char t;
+        
+        for(int i=0;i<s.length/2;i++){
+            t = s[i];
+            s[i] = s[s.length-(i+1)];
+            s[s.length-(i+1)] = t;
+        }
+        
+    }
+}
